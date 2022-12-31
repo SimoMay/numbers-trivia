3693b37b6a6157be3c4c9a94efad90dd2d106b85
Mohamed Ahmed Fouad <mohamed.af@mail.com>
2022-12-31 18:50:00 +0100

    sitemap

--- src/routes/trivia/[number]/+page.svelte
+++ src/routes/trivia/[number]/+page.svelte
@@ -10,3 +10,8 @@
 </svelte:head>
 
 <Trivia trivia={data.trivia} />
+{#if data.trivia.number >= 9999999}
+	<a href="/trivia/1">Go Back to Number 1</a>
+{:else}
+	<a href="/trivia/{data.trivia.number + 1}">Next Number</a>
+{/if}
