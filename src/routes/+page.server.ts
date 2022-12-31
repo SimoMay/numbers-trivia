35e1f00a271c21e61fb9eb055d4c75a52178794e
Mohamed Ahmed Fouad <mohamed.af@mail.com>
2022-12-31 02:30:52 +0100

    adding min & max

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,15 +1,21 @@
-
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 // 👇 this method will be invoked on BOTH the server and the client, as needed ⚠️
-export const load: PageLoad = async () => {
-  // fetch a new trivia question from http://numbersapi.com
-    const response = await fetch('http://numbersapi.com/random/trivia?json&max=9999999');
+export const load: PageServerLoad = async (event) => {
+	// get the query string parameters
+	const { url } = event;
+    const min = url.searchParams.get('min') || '0';
+    const max = url.searchParams.get('max') || '9999999';
 
-    const trivia = await response.json();
-    trivia.text = trivia.text.replace(trivia.number, 'What');
-    // return the trivia question as a JSON object
-    return {
-        trivia
-    };
-}
+	// fetch a new trivia question from http://numbersapi.com
+    const urlApi = `http://numbersapi.com/random/trivia?json&min=${min}&max=${max}`;
+    console.log(urlApi);
+	const response = await fetch(urlApi);
+
+	const trivia = await response.json();
+	trivia.text = trivia.text.replace(trivia.number, 'What');
+	// return the trivia question as a JSON object
+	return {
+		trivia
+	};
+};
