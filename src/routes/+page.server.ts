2fb8b717a5181e97e13ecf9be613367efa60a32d
Mohamed Ahmed Fouad <mohamed.af@mail.com>
2023-01-03 17:02:46 +0100

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,9 +3,7 @@ import { getTrivia } from '$lib/triviaApi';
 import { env } from '$env/dynamic/public';
 
 // 👇 this method will be invoked on BOTH the server and the client, as needed ⚠️
-export const load: PageServerLoad = async (event) => {
-	// get the query string parameters
-	const { url, params } = event;
+export const load: PageServerLoad = async ({ url, params }) => {
 	const { number } = params;
 	const min = url.searchParams.get('min') || '0';
 	const max = url.searchParams.get('max') || env.PUBLIC_MAX_TRIVIA_NUMBER;
